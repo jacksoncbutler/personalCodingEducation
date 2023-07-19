c952e88fcbc72279546777caa44c5e22d9c398fd
jacksoncbutler <jackson@butler.net>
2023-07-19 17:10:41 -0500

    first commit

---
+++ pythonRoadmap/linkedList/base.py
@@ -0,0 +1,11 @@
+
+class LinkedList:
+    class Node:
+        def __init__(self, prev, next):
+            self.prev = prev
+            self.next = next
+    
+    def __init__(self):
+        pass
+
+    
